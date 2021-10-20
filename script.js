9f3e9f8efe3882960f9e4b2ccdcd42d8a658e78a
trey-rosius <treyrosius@gmail.com>
2021-10-20 08:35:46 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * 
+ * echo "# basic_website" >> README.md
+ git init
+ git add README.md
+ git commit - m "first commit"
+ git branch - M main
+ git remote add origin https: //github.com/trey-rosius/basic_website.git
+     git push - u origin main
+ */
